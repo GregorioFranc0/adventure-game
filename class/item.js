517011503f65c5a529e4e798e1f14a7e3c52db08
gregorio <gregorio.franco1985@gmail.com>
2023-08-19 12:42:26 -0400

    building item class

--- class/item.js
+++ class/item.js
@@ -1,7 +1,9 @@
 class Item {
-  // Item Class: Item should have name and description attributes
+  constructor(name, description) {
+    this.name = name;
+    this.description = description;
+  }
 
-  // Your code here
 }
 
 module.exports = {
